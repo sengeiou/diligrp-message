82b50b0c7789d812b991aefed46884c5cdc6ac10
岳洪波 <yuehongbo@diligrp.com>
2019-04-11 17:29:04 +0800

    消息中心，对外接短信通道功能开发；短信发送功能实现。

--- message/src/main/java/com/diligrp/message/service/remote/impl/SmsChineseImpl.java
+++ message/src/main/java/com/diligrp/message/service/remote/impl/SmsChineseImpl.java
@@ -9,7 +9,6 @@ import com.dili.ss.domain.BaseOutput;
 import com.diligrp.message.common.constant.MessagePushConstant;
 import com.diligrp.message.service.remote.IMessageService;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 /**
  * <B>网建系统短信发送</B>
@@ -33,18 +32,61 @@ public class SmsChineseImpl implements IMessageService {
         params.put("smsText", object.getString(MessagePushConstant.CONTENT));
         HttpResponse response = HttpUtil.createPost(baseUrl).header(Header.CONTENT_TYPE, "application/x-www-form-urlencoded").header("charset", "utf8").form(params).execute();
         if (response.isOk()) {
-            String responseBody = response.body();
+            Integer responseCode = Integer.valueOf(response.body());
             BaseOutput output = new BaseOutput();
-            if (Integer.valueOf(responseBody) > 0) {
+            if (responseCode > 0) {
                 output.setCode(ResultCode.OK);
             } else {
-                output.setCode(responseBody);
-                output.setResult("网建短信发送返回错误，参考错误码：" + responseBody);
+                output.setCode(String.valueOf(responseCode));
+                output.setResult("网建短信发送返回错误：" + ResponseCode.getResponseCode(responseCode));
             }
             return output;
         } else {
             response.close();
             return BaseOutput.failure("调用网建返回错误").setCode(String.valueOf(response.getStatus()));
+        }
+    }
+
+    /**
+     * 网建接口返回的错误码对照
+     */
+    enum ResponseCode{
+        ACCESS_KEY_ERROR(-1, "没有该用户账户"),
+        SECRET_ERROR(-2, "接口密钥不正确"),
+        MD5_SECRET_ERROR(-21, "MD5接口密钥加密不正确"),
+        BALANCE_ERROR(-3, "短信数量不足"),
+        ACCESS_DISABLE(-11, "该用户被禁用"),
+        ILLEGAL(-14, "短信内容出现非法字符"),
+        CELLPHONE(-4, "手机号格式不正确"),
+        CELLPHONE_NON(-41, "手机号码为空"),
+        CONTENT_NON(-42, "短信内容为空"),
+        SIGN_ERROR(-51, "短信签名格式不正确,接口签名格式为：【签名内容】"),
+        SIGN_TOO_LONG(-52, "短信签名太长,建议签名10个字符以内"),
+        IP(-6, "IP限制"),
+        ;
+        private Integer code;
+        private String desc;
+
+        ResponseCode(Integer code, String desc) {
+            this.code = code;
+            this.desc = desc;
+        }
+
+        public static ResponseCode getResponseCode(Integer code) {
+            for (ResponseCode responseCode : ResponseCode.values()) {
+                if (responseCode.getCode().equals(code)) {
+                    return responseCode;
+                }
+            }
+            return null;
+        }
+
+        public Integer getCode() {
+            return code;
+        }
+
+        public String getDesc() {
+            return desc;
         }
     }
 }
