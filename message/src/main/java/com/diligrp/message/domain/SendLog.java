8e7b7a88ddf671ae8abaad94dcaa98bcb8d618ae
kelanss <466512810@qq.com>
2019-04-17 17:55:42 +0800

    按钮权限

--- message/src/main/java/com/diligrp/message/domain/SendLog.java
+++ message/src/main/java/com/diligrp/message/domain/SendLog.java
@@ -5,8 +5,9 @@ import com.dili.ss.domain.annotation.FindInSet;
 import com.dili.ss.metadata.FieldEditor;
 import com.dili.ss.metadata.annotation.EditMode;
 import com.dili.ss.metadata.annotation.FieldDef;
-import java.util.Date;
+
 import javax.persistence.*;
+import java.util.Date;
 
 /**
  * 由MyBatis Generator工具自动生成
