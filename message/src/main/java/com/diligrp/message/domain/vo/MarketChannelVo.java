4179834da157a2947f11ff642a62ce792d7819b3
kelanss <466512810@qq.com>
2019-04-18 15:06:59 +0800

    市场通道市场权限

--- message/src/main/java/com/diligrp/message/domain/vo/MarketChannelVo.java
+++ message/src/main/java/com/diligrp/message/domain/vo/MarketChannelVo.java
@@ -1,5 +1,6 @@
 package com.diligrp.message.domain.vo;
 
+import com.dili.ss.domain.annotation.Operator;
 import com.diligrp.message.domain.MarketChannel;
 
 import javax.persistence.Column;
@@ -7,37 +8,12 @@ import java.util.List;
 
 public class MarketChannelVo extends MarketChannel {
     /**
-     * 联合分组后的，通道编码，默认以,隔开
-     */
-    @Column(name = "`group_channel`")
-    private String groupChannel;
-
-    /**
-     * 联合分组后的，通道签名，默认以,隔开
-     */
-    @Column(name = "`group_signature`")
-    private String groupSignature;
-
-    /**
-     * 用户有权限的市场列表
-     */
+     *
+     * 权限市场
+     * */
+    @Operator(Operator.IN)
+    @Column(name = "`market_code`")
     private List<String> authMarkets;
-
-    public String getGroupChannel() {
-        return groupChannel;
-    }
-
-    public void setGroupChannel(String groupChannel) {
-        this.groupChannel = groupChannel;
-    }
-
-    public String getGroupSignature() {
-        return groupSignature;
-    }
-
-    public void setGroupSignature(String groupSignature) {
-        this.groupSignature = groupSignature;
-    }
 
     public List<String> getAuthMarkets() {
         return authMarkets;
