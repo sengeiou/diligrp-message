7f45f1394fac33d809abb78e635c3b5507b652ba
岳洪波 <yuehongbo@diligrp.com>
2019-07-12 11:34:59 +0800

    利刃框架版本升级，uap-sdk版本升级

--- message/src/main/java/com/diligrp/MessageApplication.java
+++ message/src/main/java/com/diligrp/MessageApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * 由MyBatis Generator工具自动生成
@@ -19,6 +20,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableScheduling
 @ComponentScan(basePackages={"com.dili.ss","com.diligrp.message","com.dili.uap.sdk"})
 @RestfulScan({"com.diligrp.message.rpc", "com.dili.uap.sdk.rpc"})
+@MapperScan({"com.dili.ss.dao"})
 /**
  * 除了内嵌容器的部署模式，Spring Boot也支持将应用部署至已有的Tomcat容器, 或JBoss, WebLogic等传统Java EE应用服务器。
  * 以Maven为例，首先需要将<packaging>从jar改成war，然后取消spring-boot-maven-plugin，然后修改Application.java
